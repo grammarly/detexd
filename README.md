beb284aa763e6868adb82adf215857f0f283d91d
Oleksii Sliusarenko <oleksii.sliusarenko@grammarly.com>
2023-06-29 18:28:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# DeTexD: A Benchmark Dataset for Delicate Text Detection
+
+This is the official repository for [DeTexD paper](TODO). Here you can find scripts used in the paper to evaluate models.
+
+See also: [DeTexD dataset](https://huggingface.co/datasets/grammarly/detexd-benchmark), [detexd-roberta-base model](https://huggingface.co/grammarly/detexd-roberta-base).
+
+## Install
+
+```sh
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run `evaluate_detexd_roberta.py` to get the published model (grammarly/detexd-roberta-base) results on published dataset (grammarly/detexd-benchmark).
+
+Run `founta_basile_comparison.ipynb` to reproduce results for models comparison from the paper. Note that you need to acquire the datsets because they have separate licences.
+
+Run `country_bias.ipynb` to reproduce country bias analysis.
+
+Run `compare_hatebert.ipynb` to reproduce hatebert models comparison.
---
+++ evaluate_detexd_roberta.py
@@ -0,0 +1,32 @@
+from transformers import pipeline
+from datasets import load_dataset
+from sklearn.metrics import precision_recall_fscore_support
+from tqdm.auto import tqdm
+from transformers.pipelines.pt_utils import KeyDataset
+
+
+def predict_binary_scores(classifier, texts):
+    # get multiclass probability scores
+    all_scores = tqdm(classifier(texts, top_k=None, truncation=True), total=len(texts))
+
+    # convert to a single score by summing the probability scores
+    # for the higher-index classes
+    return [sum(score['score']
+                for score in scores
+                if score['label'] in ('LABEL_3', 'LABEL_4', 'LABEL_5'))
+            for scores in all_scores]
+
+
+def predict_delicate(classifier, texts, threshold=0.72496545):
+    return [result > threshold for result in predict_binary_scores(classifier, texts)]
+
+
+if __name__ == '__main__':
+    dataset = load_dataset("grammarly/detexd-benchmark", split='test')
+    classifier = pipeline("text-classification", model="grammarly/detexd-roberta-base", device=0)
+    predictions = predict_delicate(classifier, KeyDataset(dataset, 'text'))
+    
+    precision, recall, f_score, _ = precision_recall_fscore_support(y_true=dataset['label'], y_pred=predictions, average='binary')
+    print(f'precision = {precision:.1%}')  # 81.4%
+    print(f'recall = {recall:.1%}')  # 78.3%
+    print(f'f_score = {f_score:.1%}')  # 79.8%
